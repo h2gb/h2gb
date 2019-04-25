592a9a674094862599f650279c902e99b70cf8a1
iagox86 <ron-git@skullsecurity.org>
2019-04-24 20:37:23 -0700

    Super simple buffer implementation with passing tests

--- src/h2buffer.rs
+++ src/h2buffer.rs
@@ -18,29 +18,52 @@ impl H2Buffer {
     H2Entry::u8(memory, i).unwrap() // TODO
   }
 
-  pub fn new(memory: Box<[u8]>) -> H2Buffer {
-    /* Create a hashmap of entries - this is kind of the core of the
-       abstraction */
-    let mut entries: HashMap<u64, H2Entry> = HashMap::new();
-
-    /* Create default entries */
-    /* TODO: Can I just use a big delete()? */
-    for (i, _b) in memory.iter().enumerate() {
-      entries.insert(i as u64, H2Buffer::default_entry(&memory, i as u64));
+  pub fn get(&self, address: u64) -> Option<&H2Entry> {
+    if address >= self.memory.len() as u64 {
+      return None;
     }
 
-    /* Create the buffer */
-    H2Buffer {
-      memory: memory,
-      entries: entries,
-      name: String::from("my name"),
-      virtual_start: 0,
+    let mut start = address;
+    while !self.entries.contains_key(&start) && start > 0{
+      start -= 1;
+    }
+
+    return self.entries.get(&start);
+  }
+
+  pub fn undefine(&mut self, address: u64, length: u64) {
+    /* Get the start of the current entry */
+    let start = match self.get(address) {
+      Some(e) => e.start,
+      None    => address,
+    };
+
+    /* Get the end of the last entry */
+    let end = match self.get(address + length - 1) {
+      Some(e) => e.start + e.length,
+      None    => address + length,
+    };
+
+    /* Loop from the left-most entry we could find to the end of the requested
+       length */
+    for i in start..end {
+      /* Create a boring default entry */
+      self.entries.insert(i, H2Buffer::default_entry(&self.memory, i));
     }
   }
 
   fn set(&mut self, entry: H2Entry) {
     let start = entry.start;
-    self.delete(start, entry.length);
+
+    /* This will undefine the starting address and anything "before" it */
+    self.undefine(start, entry.length);
+
+    /* Delete entries to make room for the object */
+    for i in start..(start+entry.length) {
+      self.entries.remove(&i);
+    }
+
+    /* Insert the new entry */
     self.entries.insert(entry.start, entry);
   }
 
@@ -62,32 +85,20 @@ impl H2Buffer {
     });
   }
 
-  pub fn delete(&mut self, address: u64, length: u64) {
-    /* If there's an object to the 'left', find it. */
-    /* TODO: I think this will go too far left though? */
-    let mut start = address;
-    while !self.entries.contains_key(&start) {
-      start -= 1;
-    }
-    self.entries.insert(start, H2Buffer::default_entry(&self.memory, start));
+  pub fn new(memory: Box<[u8]>) -> H2Buffer {
+    /* Create the buffer */
+    let mut result = H2Buffer {
+      memory: memory,
+      entries: HashMap::new(),
+      name: String::from("my name"),
+      virtual_start: 0,
+    };
 
-    /* Remove the rest of the entries */
-    for i in address..(address+length) {
-      self.entries.insert(i as u64, H2Buffer::default_entry(&self.memory, start));
-    }
-  }
+    /* Populate it with "nothing" */
+    let len = result.memory.len();
+    result.undefine(0, len as u64);
 
-  pub fn get(&self, address: u64) -> Option<&H2Entry> {
-    if address >= self.memory.len() as u64 {
-      return None;
-    }
-
-    let mut start = address;
-    while !self.entries.contains_key(&start) {
-      start -= 1;
-    }
-
-    return Some(&self.entries.get(&start).unwrap());
+    result
   }
 }
 
@@ -95,7 +106,13 @@ impl fmt::Display for H2Buffer {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let mut s = String::new();
 
-    for (_address, entry) in &self.entries {
+    /* Get a sorted list of the addresses */
+    let mut keys: Vec<&u64> = self.entries.keys().collect();
+    keys.sort();
+    let keys = keys;
+
+    for address in keys {
+      let entry = &self.entries[address];
       s.push_str(&format!("{}\n", entry));
     }
 
@@ -140,28 +157,19 @@ mod tests {
   }
 
   #[test]
-  fn test_set() {
+  fn test_basic_set() {
     let mut buffer = H2Buffer::new(Box::new([0, 1, 2, 3]));
 
-    println!("Before:");
-    println!("{}", buffer);
     buffer.set_u16(1);
-    println!();
-    println!("After:");
-    println!("{}", buffer);
-    println!();
 
-    println!("0: {}", buffer.get(0).unwrap());
     assert_eq!(buffer.get(0).unwrap().start, 0);
     assert_eq!(buffer.get(0).unwrap().length, 1);
     assert_eq!(buffer.get(0).unwrap().display, "db 00");
 
-    println!("1: {}", buffer.get(1).unwrap());
     assert_eq!(buffer.get(1).unwrap().start, 1);
     assert_eq!(buffer.get(1).unwrap().length, 2);
     assert_eq!(buffer.get(1).unwrap().display, "db 0102");
 
-    println!("2: {}", buffer.get(2).unwrap());
     assert_eq!(buffer.get(2).unwrap().start, 1);
     assert_eq!(buffer.get(2).unwrap().length, 2);
     assert_eq!(buffer.get(2).unwrap().display, "db 0102");
@@ -171,53 +179,157 @@ mod tests {
     assert_eq!(buffer.get(3).unwrap().display, "db 03");
   }
 
-  /*
   #[test]
-  fn test_set_with_overlap() {
-    let mut buffer = H2Buffer::new(Box::new([0, 1, 2, 3]));
+  fn test_set_overwrite_bigger_with_smaller() {
+    let mut buffer = H2Buffer::new(Box::new([5, 6, 7, 8]));
 
-    buffer.set(H2Entry {
-      start: 1,
-      length: 2,
-      display: String::from("test"),
-      data_refs: Box::new([]),
-      code_refs: Box::new([]),
-    });
-
-    buffer.set(H2Entry {
-      start: 2,
-      length: 2,
-      display: String::from("test"),
-      data_refs: Box::new([]),
-      code_refs: Box::new([]),
-    });
-
-    println!("{}", buffer);
+    buffer.set_u32(0);
+    buffer.set_u16(1);
 
     assert_eq!(buffer.get(0).unwrap().start, 0);
     assert_eq!(buffer.get(0).unwrap().length, 1);
-    assert_eq!(buffer.get(0).unwrap().display, "db 00");
+    assert_eq!(buffer.get(0).unwrap().display, "db 05");
 
-    for i in 2..4 {
-      assert_eq!(buffer.get(i).unwrap().start, i);
-      assert_eq!(buffer.get(i).unwrap().length, 1);
-      assert_eq!(buffer.get(i).unwrap().display, "test");
-    }
+    assert_eq!(buffer.get(1).unwrap().start, 1);
+    assert_eq!(buffer.get(1).unwrap().length, 2);
+    assert_eq!(buffer.get(1).unwrap().display, "db 0607");
 
-    assert_eq!(buffer.get(1).unwrap().start, 3);
-    assert_eq!(buffer.get(1).unwrap().length, 1);
-    assert_eq!(buffer.get(1).unwrap().display, "db 03");
+    assert_eq!(buffer.get(2).unwrap().start, 1);
+    assert_eq!(buffer.get(2).unwrap().length, 2);
+    assert_eq!(buffer.get(2).unwrap().display, "db 0607");
+
+    assert_eq!(buffer.get(3).unwrap().start, 3);
+    assert_eq!(buffer.get(3).unwrap().length, 1);
+    assert_eq!(buffer.get(3).unwrap().display, "db 08");
+  }
+
+  #[test]
+  fn test_set_with_overlap() {
+    let mut buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    buffer.set_u16(0); /* Overwritten */
+    buffer.set_u32(1); /* Overwritten */
+    buffer.set_u16(2); /* Overwritten */
+    buffer.set_u16(0); /* Stays */
+    buffer.set_u32(4); /* Stays */
+
+    assert_eq!(buffer.get(0).unwrap().start, 0);
+    assert_eq!(buffer.get(0).unwrap().length, 2);
+    assert_eq!(buffer.get(0).unwrap().display, "db 0908");
+
+    assert_eq!(buffer.get(1).unwrap().start, 0);
+    assert_eq!(buffer.get(1).unwrap().length, 2);
+    assert_eq!(buffer.get(1).unwrap().display, "db 0908");
+
+    assert_eq!(buffer.get(2).unwrap().start, 2);
+    assert_eq!(buffer.get(2).unwrap().length, 2);
+    assert_eq!(buffer.get(2).unwrap().display, "db 0706");
+
+    assert_eq!(buffer.get(3).unwrap().start, 2);
+    assert_eq!(buffer.get(3).unwrap().length, 2);
+    assert_eq!(buffer.get(3).unwrap().display, "db 0706");
+
+    assert_eq!(buffer.get(4).unwrap().start, 4);
+    assert_eq!(buffer.get(4).unwrap().length, 4);
+    assert_eq!(buffer.get(4).unwrap().display, "db 05040302");
+
+    assert_eq!(buffer.get(5).unwrap().start, 4);
+    assert_eq!(buffer.get(5).unwrap().length, 4);
+    assert_eq!(buffer.get(5).unwrap().display, "db 05040302");
+
+    assert_eq!(buffer.get(6).unwrap().start, 4);
+    assert_eq!(buffer.get(6).unwrap().length, 4);
+    assert_eq!(buffer.get(6).unwrap().display, "db 05040302");
+
+    assert_eq!(buffer.get(7).unwrap().start, 4);
+    assert_eq!(buffer.get(7).unwrap().length, 4);
+    assert_eq!(buffer.get(7).unwrap().display, "db 05040302");
+
+    assert_eq!(buffer.get(8).unwrap().start, 8);
+    assert_eq!(buffer.get(8).unwrap().length, 1);
+    assert_eq!(buffer.get(8).unwrap().display, "db 01");
   }
 
   #[test]
   fn test_bad_insert() {
+    let mut buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    /* This should simply do nothing */
+    buffer.set_u16(100);
   }
 
   #[test]
   fn test_get_out_of_bounds() {
+    let buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    /* This should return None */
+    assert_eq!(buffer.get(100),  None);
   }
 
   #[test]
-  fn test_delete() {
-  }*/
+  fn test_undefine_inside_object() {
+    let mut buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    buffer.set_u32(1);
+    buffer.undefine(2, 1);
+
+    assert_eq!(buffer.get(0).unwrap().display, "db 09");
+    assert_eq!(buffer.get(1).unwrap().display, "db 08");
+    assert_eq!(buffer.get(2).unwrap().display, "db 07");
+    assert_eq!(buffer.get(3).unwrap().display, "db 06");
+    assert_eq!(buffer.get(4).unwrap().display, "db 05");
+    assert_eq!(buffer.get(5).unwrap().display, "db 04");
+  }
+
+  #[test]
+  fn test_undefine_touches_start() {
+    let mut buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    buffer.set_u32(1);
+    buffer.undefine(0, 2);
+
+    assert_eq!(buffer.get(0).unwrap().display, "db 09");
+    assert_eq!(buffer.get(1).unwrap().display, "db 08");
+    assert_eq!(buffer.get(2).unwrap().display, "db 07");
+    assert_eq!(buffer.get(3).unwrap().display, "db 06");
+    assert_eq!(buffer.get(4).unwrap().display, "db 05");
+    assert_eq!(buffer.get(5).unwrap().display, "db 04");
+  }
+
+  #[test]
+  fn test_undefine_right_between_entries() {
+    let mut buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    buffer.set_u32(0);
+    buffer.set_u32(5);
+    buffer.undefine(4, 1);
+
+    assert_eq!(buffer.get(0).unwrap().display, "db 09080706");
+    assert_eq!(buffer.get(1).unwrap().display, "db 09080706");
+    assert_eq!(buffer.get(2).unwrap().display, "db 09080706");
+    assert_eq!(buffer.get(3).unwrap().display, "db 09080706");
+    assert_eq!(buffer.get(4).unwrap().display, "db 05");
+    assert_eq!(buffer.get(5).unwrap().display, "db 04030201");
+    assert_eq!(buffer.get(6).unwrap().display, "db 04030201");
+    assert_eq!(buffer.get(7).unwrap().display, "db 04030201");
+    assert_eq!(buffer.get(8).unwrap().display, "db 04030201");
+  }
+
+  #[test]
+  fn test_undefine_leaves_previous_and_next_entries() {
+    let mut buffer = H2Buffer::new(Box::new([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
+
+    buffer.set_u16(1);
+    buffer.set_u16(3);
+    buffer.set_u16(5);
+    buffer.undefine(3, 2);
+
+    assert_eq!(buffer.get(0).unwrap().display, "db 09");
+    assert_eq!(buffer.get(1).unwrap().display, "db 0807");
+    assert_eq!(buffer.get(2).unwrap().display, "db 0807");
+    assert_eq!(buffer.get(3).unwrap().display, "db 06");
+    assert_eq!(buffer.get(4).unwrap().display, "db 05");
+    assert_eq!(buffer.get(5).unwrap().display, "db 0403");
+    assert_eq!(buffer.get(6).unwrap().display, "db 0403");
+  }
 }
