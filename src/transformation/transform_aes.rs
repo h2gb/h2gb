3c98269b8c54782437d0706f2ed2e6d268b76379
iagox86 <ron-git@skullsecurity.org>
2021-04-02 11:49:23 -0700

    Fix IVs and write tests

--- src/transformation/transform_aes.rs
+++ src/transformation/transform_aes.rs
@@ -21,7 +21,19 @@ pub struct AESSettings {
 }
 
 impl AESSettings {
-    pub fn new(key: Vec<u8>, iv: Option<[u8; 16]>) -> SimpleResult<Self> {
+    pub fn new(key: Vec<u8>, iv: Option<Vec<u8>>) -> SimpleResult<Self> {
+        // Check the iv
+        let mut actual_iv: Option<[u8; 16]> = None;
+        if let Some(v) = iv {
+            if v.len() != 16 {
+                bail!("Invalid IV: must be 16 bytes / 128 bits long");
+            }
+
+            let mut a = [0; 16];
+            a.copy_from_slice(&v);
+            actual_iv = Some(a);
+        }
+
         let key: AESKey = match key.len() {
             16 => {
                 let mut a = [0; 16];
@@ -43,7 +55,7 @@ impl AESSettings {
 
         Ok(AESSettings {
             key: key,
-            iv: iv,
+            iv: actual_iv,
         })
     }
 }
@@ -125,25 +137,74 @@ mod tests {
 
     #[test]
     fn test_aes() -> SimpleResult<()> {
-        let result = Transformation::FromAES(AESSettings::new(
-            b"AAAAAAAAAAAAAAAA".to_vec(),
-            None,
-        )?).transform(&b"\x6c\x97\x52\xb3\x06\xde\xc3\xaa\x5d\x4d\x0e\xe7\x98\xcc\xd9\xb0".to_vec())?;
-        assert_eq!(b"Hello world!".to_vec(), result);
+        let tests: Vec<(Vec<u8>, Vec<u8>, Option<Vec<u8>>, Vec<u8>)> = vec![
+            (
+                b"Test for AES-128 with CBC padding and a couple blocks".to_vec(),            // Plaintext
+                b"AAAAAAAAAAAAAAAA".to_vec(),                                                 // Key
+                None,                                                                         // IV
+                // Ciphertext
+                b"\x8f\x43\x5a\x89\xf4\xda\x6b\x67\xe2\x2f\x43\xaf\x71\xbf\x93\xb0\
+                  \x21\x2c\x88\x77\x01\x5c\x28\xe9\xa6\xac\x34\xb8\xb4\x3c\x15\x21\
+                  \xa3\x36\x13\xbd\x14\x25\xe3\x47\x37\x87\xa0\x0d\x65\xd0\x15\xe9\
+                  \x5e\xf8\x64\x3d\x91\x86\x21\xc4\x3e\x97\x24\x4f\xb6\xa3\x27\xf5".to_vec(),
+            ),
 
+            (
+                b"AES128 with an IV!".to_vec(),                                               // Plaintext
+                b"AAAAAAAAAAAAAAAA".to_vec(),                                                 // Key
+                Some(b"BBBBBBBBBBBBBBBB".to_vec()),                                           // IV
+                // Ciphertext
+                b"\x86\x62\x63\x07\x47\x5d\x2e\x61\x8e\x3d\xed\x1a\xff\x00\xef\xc3\
+                  \x95\x8b\x83\x3d\xc8\x30\x6e\x50\x36\x4e\x6d\x29\x9e\x19\xd2\xc9".to_vec(),
+            ),
 
-        let result = Transformation::FromAES(AESSettings::new(
-            b"AAAAAAAAAAAAAAAAAAAAAAAA".to_vec(),
-            None,
-        )?).transform(&b"\xc8\xcc\x26\xe8\x1a\x48\x8e\xb0\x1e\xac\xb1\xc5\x7c\x07\xe3\x30\xa7\xda\x88\x27\xbf\xcc\x1e\xab\xcc\x53\xd5\x0a\x21\x55\x93\x79".to_vec())?;
-        assert_eq!(b"Hello world! This is a test".to_vec(), result);
+            (
+                b"Test for AES-192 with CBC padding!".to_vec(),                               // Plaintext
+                b"AAAAAAAAAAAAAAAAAAAAAAAA".to_vec(),                                         // Key
+                None,                                                                         // IV
+                // Ciphertext
+                b"\x4d\x44\x10\x2e\x61\x88\xe9\xa0\xc5\xf0\x60\xd9\xb7\x0c\xc6\x75\
+                  \xed\xcf\x7c\xf3\xaa\xe0\xdb\xcc\x39\xd7\x7f\x24\x02\x6d\x6c\x98\
+                  \x5d\x34\x46\xc6\x76\x19\xaa\xca\x21\xdc\x7d\x09\x3e\x23\x56\x4a".to_vec(),
+            ),
 
-        let result = Transformation::FromAES(AESSettings::new(
-            b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".to_vec(),
-            None,
-        )?).transform(&b"\xed\x79\xa2\x28\x21\x55\x65\xc9\x50\xbc\x93\xc8\xa3\xed\x6a\xc4\xac\x6c\x8c\x56\x56\xea\x83\x29\x22\x43\x76\xa1\xe2\x2d\x74\xe3\x1d\xcf\x51\x5c\xdc\xc6\x34\x4c\xa6\x5b\xb4\x46\x8b\x52\x3c\xa2".to_vec())?;
-        assert_eq!(b"Final test for AES-256 with a longer plaintext".to_vec(), result);
+            (
+                b"Final test for AES-256 with a longer plaintext".to_vec(),                   // Plaintext
+                b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".to_vec(),                                 // Key
+                None,                                                                         // IV
+                // Ciphertext
+                b"\xed\x79\xa2\x28\x21\x55\x65\xc9\x50\xbc\x93\xc8\xa3\xed\x6a\xc4\
+                  \xac\x6c\x8c\x56\x56\xea\x83\x29\x22\x43\x76\xa1\xe2\x2d\x74\xe3\
+                  \x1d\xcf\x51\x5c\xdc\xc6\x34\x4c\xa6\x5b\xb4\x46\x8b\x52\x3c\xa2".to_vec(),
+            ),
+
+            (
+                b"AES256 with an all-C IV!".to_vec(),                                         // Plaintext
+                b"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA".to_vec(),                                 // Key
+                Some(b"CCCCCCCCCCCCCCCC".to_vec()),                                           // IV
+                // Ciphertext
+                b"\x99\x70\x25\x50\x5c\xd5\x9e\x9d\xc7\x73\x19\x94\x5c\xae\xc9\x9f\
+                  \xd5\x28\x00\xf1\x34\xcd\xcf\xf9\xbf\x15\x08\x52\x2b\xd4\x09\xa2".to_vec(),
+            ),
+        ];
+
+        for (plaintext, key, iv, ciphertext) in tests {
+            let result = Transformation::FromAES(AESSettings::new(
+                key,
+                iv,
+            )?).transform(&ciphertext)?;
+            assert_eq!(plaintext, result);
+        }
 
         Ok(())
     }
+
+    // #[test]
+    // fn test_aes_ecb() -> SimpleResult<()> {
+    //     bail!("Not implemented");
+    // }
+    // #[test]
+    // fn test_aes_errors() -> SimpleResult<()> {
+    //     bail!("Not implemented");
+    // }
 }
