90cb597711ffc4c494e8be38cd03b293c5ae7b55
iagox86 <ron-git@skullsecurity.org>
2021-04-21 13:38:13 -0700

    Add the 'oldactions' folder (the code therein isn't actually used, but I need it for later)

---
+++ src/project/oldactions/buffer_clone_shallow.rs
@@ -0,0 +1,211 @@
+//! An action that clones part of a buffer to a new name.
+//!
+//! Preserves the data and base address, but nothing else. Can be edited and
+//! stuff, like any buffer, but can't be merged back (since it's a copy).
+//!
+//! Copies the original base_address unless a custom one is passed in.
+
+use redo::Command;
+use serde::{Serialize, Deserialize};
+use simple_error::{SimpleResult, SimpleError, bail};
+
+use crate::project::h2project::H2Project;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ActionBufferCloneShallowForward {
+    pub clone_from_name: String,
+    pub clone_to_name: String,
+    pub new_base_address: Option<usize>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ActionBufferCloneShallowBackward {
+    clone_from_name: String,
+    clone_to_name: String,
+    new_base_address: Option<usize>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ActionBufferCloneShallow {
+    forward: Option<ActionBufferCloneShallowForward>,
+    backward: Option<ActionBufferCloneShallowBackward>,
+}
+
+impl ActionBufferCloneShallow {
+    pub fn new(forward: ActionBufferCloneShallowForward) -> Self {
+        ActionBufferCloneShallow {
+            forward: Some(forward),
+            backward: None,
+        }
+    }
+}
+
+impl From<(&str, &str, Option<usize>)> for ActionBufferCloneShallow {
+    fn from(o: (&str, &str, Option<usize>)) -> Self {
+        ActionBufferCloneShallow {
+            forward: Some(ActionBufferCloneShallowForward {
+                clone_from_name: o.0.to_string(),
+                clone_to_name: o.1.to_string(),
+                new_base_address: o.2,
+            }),
+            backward: None,
+        }
+    }
+}
+
+impl Command for ActionBufferCloneShallow {
+    type Target = H2Project;
+    type Error = SimpleError;
+
+    fn apply(&mut self, project: &mut H2Project) -> SimpleResult<()> {
+        // Get the forward instructions
+        let forward = match &self.forward {
+            Some(f) => f,
+            None => bail!("Failed to apply: missing context"),
+        };
+
+        // Make sure we'll be able to insert - this is mostly to save the
+        // cloning time
+        if project.buffer_exists(&forward.clone_to_name) {
+            bail!("Buffer already exists: {}", forward.clone_to_name);
+        }
+
+        let original = project.get_buffer(&forward.clone_from_name)?;
+        let new_buffer = original.clone_shallow(forward.new_base_address)?;
+        project.buffer_insert(&forward.clone_to_name, new_buffer)?;
+
+        // Populate backward for undo
+        self.backward = Some(ActionBufferCloneShallowBackward {
+            clone_to_name: forward.clone_to_name.clone(),
+            clone_from_name: forward.clone_from_name.clone(),
+            new_base_address: forward.new_base_address,
+        });
+        self.forward = None;
+
+        Ok(())
+    }
+
+    fn undo(&mut self, project: &mut H2Project) -> SimpleResult<()> {
+        let backward = match &self.backward {
+            Some(b) => b,
+            None => bail!("Failed to undo: missing context"),
+        };
+
+        // We don't need to keep a copy of the buffer since the name is enough
+        // to clone it
+        project.buffer_remove(&backward.clone_to_name)?;
+
+        self.forward = Some(ActionBufferCloneShallowForward {
+            clone_to_name: backward.clone_to_name.clone(),
+            clone_from_name: backward.clone_from_name.clone(),
+            new_base_address: backward.new_base_address,
+        });
+        self.backward = None;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+    use redo::Record;
+    use simple_error::SimpleResult;
+
+    use crate::transformation::TransformHex;
+
+    use crate::project::actions::Action;
+    use crate::project::h2project::H2Project;
+
+    #[test]
+    fn test_action() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a buffer with some data
+        record.apply(Action::buffer_create_from_bytes("buffer", b"AAAAAAAAAA".to_vec(), 0x80000000))?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Clone it
+        record.apply(Action::buffer_clone_shallow("buffer", "newbuffer", None))?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("newbuffer")?.data);
+
+        // Edit the original and ensure the change isn't reflected
+        record.apply(Action::buffer_edit("buffer", b"BBBBBB".to_vec(), 2))?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("newbuffer")?.data);
+
+        // Undo the edit
+        record.undo()?;
+
+        // Undo the clone
+        record.undo()?;
+        assert_eq!(false, record.target().buffer_exists("newbuffer"));
+        assert!(record.target().get_buffer("newbuffer").is_err());
+
+        record.redo()?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("newbuffer")?.data);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_base_address() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a buffer with some data
+        record.apply(Action::buffer_create_from_bytes("buffer", b"AAAAAAAAAA".to_vec(), 0x80000000))?;
+
+        // Clone it with no base address, it should have the same one
+        record.apply(Action::buffer_clone_shallow("buffer", "newbuffer", None))?;
+        assert_eq!(0x80000000, record.target().get_buffer("newbuffer")?.base_address);
+
+        // Clone it again, this time customize the base address
+        record.apply(Action::buffer_clone_shallow("buffer", "newbuffer2", Some(10)))?;
+        assert_eq!(10, record.target().get_buffer("newbuffer2")?.base_address);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_action_fails_on_bad_change() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a buffer with some data
+        record.apply(Action::buffer_create_from_bytes("buffer", b"AAAAAAAAAA".to_vec(), 0x80000000))?;
+
+        // Rename it to itself (invalid)
+        assert!(record.apply(Action::buffer_clone_shallow("buffer", "buffer", None)).is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_untransform_is_preserved() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a simple buffer
+        record.apply(Action::buffer_create_from_bytes("buffer", b"4a4B4c4D4e".to_vec(), 0x80000000))?;
+
+        // Transform it
+        record.apply(Action::buffer_transform("buffer", TransformHex::new()))?;
+
+        // Clone
+        record.apply(Action::buffer_clone_shallow("buffer", "newbuffer", None))?;
+        assert_eq!(b"JKLMN".to_vec(), record.target().get_buffer("newbuffer")?.data);
+
+        // Ensure that untransform works on the new buffer
+        record.apply(Action::buffer_untransform("newbuffer"))?;
+        assert_eq!(b"4a4b4c4d4e".to_vec(), record.target().get_buffer("newbuffer")?.data);
+        assert_eq!(b"JKLMN".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        Ok(())
+    }
+}
---
+++ src/project/oldactions/buffer_edit.rs
@@ -0,0 +1,230 @@
+//! Replace part of a buffer's data, maintaining the original length.
+
+use redo::Command;
+use serde::{Serialize, Deserialize};
+use simple_error::{SimpleResult, SimpleError, bail};
+
+use crate::project::h2project::H2Project;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ActionBufferEditForward {
+    pub name: String,
+    pub new_data: Vec<u8>,
+    pub offset: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ActionBufferEditBackward {
+    name: String,
+    original_data: Vec<u8>,
+    offset: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ActionBufferEdit {
+    forward: Option<ActionBufferEditForward>,
+    backward: Option<ActionBufferEditBackward>,
+}
+
+impl ActionBufferEdit {
+    pub fn new(forward: ActionBufferEditForward) -> Self {
+        ActionBufferEdit {
+            forward: Some(forward),
+            backward: None,
+        }
+    }
+}
+
+impl From<(&str, Vec<u8>, usize)> for ActionBufferEdit {
+    fn from(o: (&str, Vec<u8>, usize)) -> Self {
+        ActionBufferEdit {
+            forward: Some(ActionBufferEditForward {
+                name: o.0.to_string(),
+                new_data: o.1,
+                offset: o.2,
+            }),
+            backward: None,
+        }
+    }
+}
+
+impl Command for ActionBufferEdit {
+    type Target = H2Project;
+    type Error = SimpleError;
+
+    fn apply(&mut self, project: &mut H2Project) -> SimpleResult<()> {
+        // Get the forward instructions
+        let forward = match &self.forward {
+            Some(f) => f,
+            None => bail!("Failed to apply: missing context"),
+        };
+
+        // Apply the change
+        let buffer = project.get_buffer_mut(&forward.name)?;
+        let original_data = buffer.edit(forward.new_data.clone(), forward.offset)?;
+
+        // Populate backward for undo
+        self.backward = Some(ActionBufferEditBackward {
+            name: forward.name.clone(),
+            original_data: original_data,
+            offset: forward.offset,
+        });
+        self.forward = None;
+
+        Ok(())
+    }
+
+    fn undo(&mut self, project: &mut H2Project) -> SimpleResult<()> {
+        let backward = match &self.backward {
+            Some(b) => b,
+            None => bail!("Failed to undo: missing context"),
+        };
+
+        let buffer = project.get_buffer_mut(&backward.name)?;
+        let new_data = buffer.edit(backward.original_data.clone(), backward.offset)?;
+
+        self.forward = Some(ActionBufferEditForward {
+            name: backward.name.clone(),
+            new_data: new_data,
+            offset: backward.offset,
+        });
+        self.backward = None;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use simple_error::SimpleResult;
+
+    use crate::project::h2project::H2Project;
+    use redo::Record;
+    use pretty_assertions::assert_eq;
+    use crate::project::actions::Action;
+
+    #[test]
+    fn test_action() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a buffer with some data
+        record.apply(Action::buffer_create_from_bytes("buffer", b"AAAAAAAAAA".to_vec(), 0x80000000))?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change the middle three bytes
+        record.apply(Action::buffer_edit("buffer", b"BBBBBB".to_vec(), 2))?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change the last two bytes
+        record.apply(Action::buffer_edit("buffer", b"CC".to_vec(), 8))?;
+        assert_eq!(b"AABBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change the first two bytes
+        record.apply(Action::buffer_edit("buffer", b"EE".to_vec(), 0))?;
+        assert_eq!(b"EEBBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change the first one byte
+        record.apply(Action::buffer_edit("buffer", b"F".to_vec(), 0))?;
+        assert_eq!(b"FEBBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change the last one byte
+        record.apply(Action::buffer_edit("buffer", b"G".to_vec(), 9))?;
+        assert_eq!(b"FEBBBBBBCG".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change the whole vector
+        record.apply(Action::buffer_edit("buffer", b"ZZZZZZZZZZ".to_vec(), 0))?;
+        assert_eq!(b"ZZZZZZZZZZ".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Make sure each step can undo
+        record.undo()?;
+        assert_eq!(b"FEBBBBBBCG".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.undo()?;
+        assert_eq!(b"FEBBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.undo()?;
+        assert_eq!(b"EEBBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.undo()?;
+        assert_eq!(b"AABBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.undo()?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.undo()?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Make sure each step can redo
+        record.redo()?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.redo()?;
+        assert_eq!(b"AABBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.redo()?;
+        assert_eq!(b"EEBBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.redo()?;
+        assert_eq!(b"FEBBBBBBCC".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.redo()?;
+        assert_eq!(b"FEBBBBBBCG".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_action_fails_on_bad_change() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a buffer with some data
+        record.apply(Action::buffer_create_from_bytes("buffer", b"AAAAAAAAAA".to_vec(), 0x80000000))?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change some middle bytes so we can test undo
+        record.apply(Action::buffer_edit("buffer", b"BBBBBB".to_vec(), 2))?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change one too many bytes from the start
+        assert!(record.apply(Action::buffer_edit("buffer", b"AAAAAAAAAAB".to_vec(), 0)).is_err());
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change one too many bytes from the end
+        assert!(record.apply(Action::buffer_edit("buffer", b"AB".to_vec(), 9)).is_err());
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change something completely off the end
+        assert!(record.apply(Action::buffer_edit("buffer", b"AB".to_vec(), 100)).is_err());
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Make sure undo and redo are correct
+        record.undo()?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.redo()?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_action_fails_on_zero_size() -> SimpleResult<()> {
+        let mut record: Record<Action> = Record::new(
+            H2Project::new("name", "1.0")
+        );
+
+        // Create a buffer with some data
+        record.apply(Action::buffer_create_from_bytes("buffer", b"AAAAAAAAAA".to_vec(), 0x80000000))?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change some middle bytes so we can test undo
+        record.apply(Action::buffer_edit("buffer", b"BBBBBB".to_vec(), 2))?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Change zero bytes, make sure it fails
+        assert!(record.apply(Action::buffer_edit("buffer", b"".to_vec(), 0)).is_err());
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        // Make sure undo and redo are correct
+        record.undo()?;
+        assert_eq!(b"AAAAAAAAAA".to_vec(), record.target().get_buffer("buffer")?.data);
+        record.redo()?;
+        assert_eq!(b"AABBBBBBAA".to_vec(), record.target().get_buffer("buffer")?.data);
+
+        Ok(())
+    }
+}
