46f3e93ae90cdec6d9b76432a92fcd77f23caa83
iagox86 <ron-git@skullsecurity.org>
2021-06-06 15:55:33 -0700

    Write new documentation for the generic_number module

--- src/generic_number/mod.rs
+++ src/generic_number/mod.rs
@@ -1,62 +1,40 @@
-//! A simple library for reading differently-sized integers and floats.
+//! A library for reading and formatting differently-sized integers and floats.
 //!
-//! While writing h2gb, I needed a way to dynamically read integers from a
-//! Vec of u8 bytes. Libraries like [`byteorder`] and [`io::Cursor`] nearly
-//! have the right functionality, but weren't quite flexible enough.
+//! The bulk of functionality is split into a couple parts:
 //!
-//! This library defines a [`Context`] type, which is just a thin wrapper for a
-//! [`std::io::Cursor`]. It's also a super cheap type that can be cloned as
-//! needed.
+//! * [`GenericReader`] - Reads any primitive type from a [`Context`]
+//! * [`GenericNumber`] - Represents any primitive type
+//! * [`GenericFormatter`] - Renders a [`GenericNumber`] with user-configurable options
 //!
-//! To create an instance, you need to first define how the number will be
-//! defined. Using the [`GenericReader`] enum, choose a size and a
-//! [`Endian`]:
+//! Both [`GenericNumber`] and [`GenericFormatter`] are serializable, which is
+//! what makes it really useful for h2gb!
 //!
-//! ```
-//! use libh2gb::generic_number::{GenericReader, Endian};
+//! # Usage
 //!
-//! let d = GenericReader::U32(Endian::Big);
-//! ```
-//!
-//! Once you have an instance of [`GenericReader`], it can convert a
-//! [`Context`] into a string in a variety of formats - use `GenericFormatter` to
-//! configure how it should convert:
+//! To use, you typically read a value from a buffer using a [`GenericReader`],
+//! then display it using a [`GenericFormatter`]:
 //!
 //! ```
 //! use libh2gb::generic_number::*;
 //!
-//! let buffer = b"ABCD".to_vec();
+//! // Create a buffer
+//! let buffer = b"\x01\x23\x45\x67".to_vec();
+//!
+//! // Create a context that points to the start of the buffer
 //! let context = Context::new_at(&buffer, 0);
-//! let d = GenericReader::U32(Endian::Big);
-//! let number = d.read(context).unwrap();
 //!
-//! assert_eq!("0x41424344", HexFormatter::pretty().render(number).unwrap());
-//! assert_eq!("1094861636", DecimalFormatter::new().render(number).unwrap());
-//! assert_eq!("0o10120441504", OctalFormatter::pretty().render(number).unwrap());
-//! assert_eq!("0b01000001010000100100001101000100", BinaryFormatter::pretty().render(number).unwrap());
-//! assert_eq!("1.094861636e9", ScientificFormatter::pretty().render(number).unwrap());
-//! ```
+//! // Create a reader that knows how to read a U32 big endian value
+//! let reader = GenericReader::U32(Endian::Big);
 //!
-//! The string conversion is designed to be "stamp"-able - you can define the
-//! format once, then apply it in multiple places or with multiple formats! No
-//! context or data is stored as part of the type.
+//! // Read from the context into a generic number
+//! let number = reader.read(context).unwrap();
 //!
-//! In addition to formatting a string, 64-bit and smaller unsigned instances
-//! of [`GenericReader`] can be converted into [`u64`] (unsigned) integers, and
-//! 64-bit and smaller signed instances can be converted into [`i64`] (signed)
-//! integers:
-//!
-//! ```
-//! use libh2gb::generic_number::{Context, GenericReader, Endian, GenericFormatter, HexFormatter, BinaryFormatter, ScientificFormatter};
-//!
-//! let buffer = b"\x01\x02\x03\x04\x05\x06\x07\x08".to_vec();
-//! let context = Context::new_at(&buffer, 0);
-//! let d = GenericReader::U32(Endian::Big);
-//!
-//! assert_eq!(0x01,               GenericReader::U8.read(context).unwrap().as_u64().unwrap());
-//! assert_eq!(0x0102,             GenericReader::U16(Endian::Big).read(context).unwrap().as_u64().unwrap());
-//! assert_eq!(0x01020304,         GenericReader::U32(Endian::Big).read(context).unwrap().as_u64().unwrap());
-//! assert_eq!(0x0102030405060708, GenericReader::U64(Endian::Big).read(context).unwrap().as_u64().unwrap());
+//! // Display it using different formatters (these use the pretty defaults)
+//! assert_eq!("0x01234567",                         HexFormatter::pretty().render(number).unwrap());
+//! assert_eq!("19088743",                           DecimalFormatter::new().render(number).unwrap());
+//! assert_eq!("0o110642547",                        OctalFormatter::pretty().render(number).unwrap());
+//! assert_eq!("0b00000001001000110100010101100111", BinaryFormatter::pretty().render(number).unwrap());
+//! assert_eq!("1.9088743e7",                        ScientificFormatter::pretty().render(number).unwrap());
 //! ```
 
 mod helpers;
