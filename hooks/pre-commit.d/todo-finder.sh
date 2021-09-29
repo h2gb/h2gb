1ae91c1044244f02afa8d0b0cc144bfeaadeb3d8
iagox86 <ron-git@skullsecurity.org>
2021-09-29 14:09:21 -0700

    Fix todo-finder to understand the workspace structure

--- hooks/pre-commit.d/todo-finder.sh
+++ hooks/pre-commit.d/todo-finder.sh
@@ -3,16 +3,16 @@
 set -euo pipefail
 
 # Get the root directory
-BASE=$(git rev-parse --show-toplevel)/src
+BASE=$(git rev-parse --show-toplevel)/
 
 # Turn on yellow output + grep for TODO
 echo -ne '\e[33m' # Yellow
-egrep -r 'TODO' $BASE/ || true # Don't die if there are none
+egrep -r 'TODO' $BASE/ --include=**.rs || true # Don't die if there are none
 echo -ne '\e[0m' # No formatting
 
 # Turn on red output + grep for XXX
 echo -ne '\e[31m' # Yellow
-egrep -r 'XXX' $BASE/ || true # Don't die if there are none
+egrep -r 'XXX' $BASE/ --include=**.rs || true # Don't die if there are none
 echo -ne '\e[0m' # No formatting
 
 # Never block
