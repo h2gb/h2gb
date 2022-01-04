8a42269ce62d4a1cd33631a6a269d07c3010f984
iagox86 <ron-git@skullsecurity.org>
2022-01-04 15:02:14 -0800

    Finish an implementation of H2Constant

--- h2data/src/ng/h2constants.rs
+++ h2data/src/ng/h2constants.rs
@@ -1,16 +1,16 @@
-use std::cmp::Eq;
 use std::fs::File;
-use std::hash::Hash;
 use std::io;
+use std::path::PathBuf;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 use bimap::BiMap;
-use serde::de::DeserializeOwned;
 use simple_error::{SimpleResult, SimpleError, bail};
 
 use generic_number::Integer;
 
 /// A named collection of constants
+#[derive(Debug)]
 pub struct H2Constants {
     constants: BiMap<String, Integer>,
 }
@@ -50,7 +50,9 @@ impl H2Constants {
                 SimpleError::new(format!("Couldn't parse second CSV field as integer"))
             })?;
 
-            out.insert(name, number);
+            out.insert_no_overwrite(name, number).map_err(|e| {
+                SimpleError::new(format!("Duplicate key or value while reading CSV: {:?}", e))
+            })?;
         }
 
         Ok(out)
@@ -62,7 +64,7 @@ impl H2Constants {
         })
     }
 
-    pub fn load_from_csv_file(filename: &str) -> SimpleResult<Self> {
+    pub fn load_from_csv_file(filename: &PathBuf) -> SimpleResult<Self> {
         Ok(Self {
             constants: Self::read_csv(io::BufReader::new(File::open(filename).map_err(|e| {
                 SimpleError::new(format!("Could not read file: {}", e))
@@ -70,13 +72,49 @@ impl H2Constants {
         })
     }
 
+    pub fn to_csv(&self) -> SimpleResult<String> {
+        // Convert to String->String
+        let mut w = csv::WriterBuilder::new().has_headers(false).from_writer(vec![]);
+
+        for (k, v) in &self.constants {
+            w.write_record(&[k.clone(), v.to_string()]).map_err(|e| {
+                SimpleError::new(format!("Could not create CSV record: {:?}", e))
+            })?;
+        }
+
+        let bytes = w.into_inner().map_err(|e| {
+            SimpleError::new(format!("Couldn't write CSV: {:?}", e))
+        })?;
+
+        String::from_utf8(bytes).map_err(|e| {
+            SimpleError::new(format!("Couldn't write CSV: {:?}", e))
+        })
+
+    }
+
     fn read_yaml<R>(reader: R) -> SimpleResult<BiMap<String, Integer>>
     where
         R: io::Read
     {
-        Ok(serde_yaml::from_reader(reader).map_err(|e| {
-            SimpleError::from(e)
-        })?)
+        // Read as String->String
+        let h: HashMap<String, String> = serde_yaml::from_reader(reader).map_err(|e| {
+            SimpleError::new(format!("Couldn't read YAML file as String->String mapping: {:?}", e))
+        })?;
+
+        // Convert to String->Integer
+        let mut out: BiMap<String, Integer> = BiMap::new();
+        for (k, v) in h.into_iter() {
+            // Get the integer
+            let i = Integer::from_str(&v).map_err(|e| {
+                SimpleError::new(format!("Couldn't parse integer from YAML: {:?}", e))
+            })?;
+
+            out.insert_no_overwrite(k, i).map_err(|e| {
+                SimpleError::new(format!("Duplicate key or value while reading YAML: {:?}", e))
+            })?;
+        }
+
+        Ok(out)
     }
 
     pub fn load_from_yaml_string(data: &str) -> SimpleResult<Self> {
@@ -85,7 +123,7 @@ impl H2Constants {
         })
     }
 
-    pub fn load_from_yaml_file(filename: &str) -> SimpleResult<Self> {
+    pub fn load_from_yaml_file(filename: &PathBuf) -> SimpleResult<Self> {
         Ok(Self {
             constants: Self::read_yaml(io::BufReader::new(File::open(filename).map_err(|e| {
                 SimpleError::new(format!("Could not read file: {}", e))
@@ -93,13 +131,42 @@ impl H2Constants {
         })
     }
 
+    pub fn to_yaml(&self) -> SimpleResult<String> {
+        // Convert to String->String
+        let mut h: HashMap<String, String> = HashMap::new();
+
+        for (k, v) in &self.constants {
+            h.insert(k.clone(), v.to_string());
+        }
+
+        serde_yaml::to_string(&h).map_err(|e| {
+            SimpleError::new(format!("Failed to serialize to YAML: {}", e))
+        })
+    }
+
     fn read_json<R>(reader: R) -> SimpleResult<BiMap<String, Integer>>
     where
         R: io::Read
     {
-        Ok(serde_json::from_reader(reader).map_err(|e| {
-            SimpleError::from(e)
-        })?)
+        // Read as String->String
+        let h: HashMap<String, String> = serde_json::from_reader(reader).map_err(|e| {
+            SimpleError::new(format!("Couldn't read JSON file as String->String mapping: {:?}", e))
+        })?;
+
+        // Convert to String->Integer
+        let mut out: BiMap<String, Integer> = BiMap::new();
+        for (k, v) in h.into_iter() {
+            // Get the integer
+            let i = Integer::from_str(&v).map_err(|e| {
+                SimpleError::new(format!("Couldn't parse integer from JSON: {:?}", e))
+            })?;
+
+            out.insert_no_overwrite(k, i).map_err(|e| {
+                SimpleError::new(format!("Duplicate key or value while reading JSON: {:?}", e))
+            })?;
+        }
+
+        Ok(out)
     }
 
     pub fn load_from_json_string(data: &str) -> SimpleResult<Self> {
@@ -108,11 +175,24 @@ impl H2Constants {
         })
     }
 
-    pub fn load_from_json_file(filename: &str) -> SimpleResult<Self> {
+    pub fn load_from_json_file(filename: &PathBuf) -> SimpleResult<Self> {
         Ok(Self {
             constants: Self::read_json(io::BufReader::new(File::open(filename).map_err(|e| {
                 SimpleError::new(format!("Could not read file: {}", e))
             })?))?
+        })
+    }
+
+    pub fn to_json(&self) -> SimpleResult<String> {
+        // Convert to String->String
+        let mut h: HashMap<String, String> = HashMap::new();
+
+        for (k, v) in &self.constants {
+            h.insert(k.clone(), v.to_string());
+        }
+
+        serde_json::to_string_pretty(&h).map_err(|e| {
+            SimpleError::new(format!("Failed to serialize to JSON: {}", e))
         })
     }
 
@@ -152,59 +232,137 @@ mod tests {
         // Non-numbers fail
         assert!(H2Constants::load_from_csv_string("100,TEST1\n").is_err());
 
-        // Blank lines succeedd
+        // Blank lines are ignored
         assert_eq!(2, H2Constants::load_from_csv_string("TEST1,100\n\n\n\n\nTEST3,200\n")?.len());
+
+        // Duplicates fail in both directions
+        assert!(H2Constants::load_from_csv_string("TEST1,1\nTEST2,1\n").is_err());
+        assert!(H2Constants::load_from_csv_string("TEST1,1\nTEST1,2\n").is_err());
+
+        // Check if we can convert it back and forth
+        let data = constants.to_csv()?;
+        let constants = H2Constants::load_from_csv_string(&data)?;
+
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
 
         Ok(())
     }
 
     #[test]
     fn test_csv_file() -> SimpleResult<()> {
-        Ok(())
-    }
+        // Load the data
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/test1.csv");
 
-    #[test]
-    fn test_csv_errors() -> SimpleResult<()> {
+        let constants = H2Constants::load_from_csv_file(&d)?;
+
+        // Do all the same tests as test_csv()
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
         Ok(())
     }
 
     #[test]
     fn test_csv_empty() -> SimpleResult<()> {
+        assert_eq!(0, H2Constants::load_from_csv_string("")?.len());
+
         Ok(())
     }
 
     #[test]
     fn test_json() -> SimpleResult<()> {
+        let data = "{ \"TEST1\": \"1\", \"TEST3\": \"5\", \"TEST2\": \"100\", \"TEST4\": \"-10000\", \"TEST5\": \"0x100\" }";
+        let constants: H2Constants = H2Constants::load_from_json_string(data)?;
+
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
+        // Check if we can convert it back and forth
+        let data = constants.to_json()?;
+        let constants = H2Constants::load_from_json_string(&data)?;
+
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
         Ok(())
     }
 
     #[test]
     fn test_json_file() -> SimpleResult<()> {
-        Ok(())
-    }
+        // Load the data
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/test2.json");
 
-    #[test]
-    fn test_json_invalid() -> SimpleResult<()> {
+        let constants = H2Constants::load_from_json_file(&d)?;
+
+        // Do all the same tests as test_json()
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
         Ok(())
     }
 
     #[test]
     fn test_yaml() -> SimpleResult<()> {
+        let data = "---
+TEST4: -10000
+TEST1: 1
+TEST2: 100
+TEST3: 5
+TEST5: 256";
+
+        let constants: H2Constants = H2Constants::load_from_yaml_string(data)?;
+
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
+        let data = constants.to_yaml()?;
+        let constants = H2Constants::load_from_yaml_string(&data)?;
+
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
         Ok(())
     }
 
     #[test]
     fn test_yaml_file() -> SimpleResult<()> {
-        Ok(())
-    }
+        // Load the data
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/test3.yaml");
 
-    #[test]
-    fn test_yaml_invalid() -> SimpleResult<()> {
-        Ok(())
-    }
+        let constants = H2Constants::load_from_yaml_file(&d)?;
 
-    #[test]
-    fn test_duplicates() -> SimpleResult<()> {
+        // Do all the same tests as test_yaml()
+        assert_eq!(Some(&Integer::from(1u32)), constants.get_by_name("TEST1"));
+        assert_eq!(Some(&Integer::from(100u32)), constants.get_by_name("TEST2"));
+        assert_eq!(Some(&Integer::from(5u8)), constants.get_by_name("TEST3"));
+        assert_eq!(Some(&Integer::from(-10000i32)), constants.get_by_name("TEST4"));
+        assert_eq!(Some(&Integer::from(0x100u32)), constants.get_by_name("TEST5"));
+
         Ok(())
     }
 }
