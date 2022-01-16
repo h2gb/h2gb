6815a4bae6c53bd447ad85d4f7e8a883eeaf7a32
iagox86 <ron-git@skullsecurity.org>
2022-01-16 12:25:11 -0800

    Write tests for datafile loading, and fix a couple bugs

--- h2datatype/src/data/mod.rs
+++ h2datatype/src/data/mod.rs
@@ -56,10 +56,10 @@ impl FileType {
 
 #[derive(Debug)]
 pub struct DataNg {
-    constants: HashMap<String, Constants>,
-    enums:     HashMap<String, Enums>,
-    bitmasks:  HashMap<String, Bitmasks>,
-    types:     HashMap<String, Types>,
+    pub constants: HashMap<String, Constants>,
+    pub enums:     HashMap<String, Enums>,
+    pub bitmasks:  HashMap<String, Bitmasks>,
+    pub types:     HashMap<String, Types>,
 }
 
 impl DataNg {
@@ -69,15 +69,6 @@ impl DataNg {
             enums:     HashMap::new(),
             bitmasks:  HashMap::new(),
             types:     HashMap::new(),
-        }
-    }
-
-    fn load_file<T: DataTrait>(path: &Path) -> SimpleResult<T> {
-        match FileType::from_filename(path) {
-            Some(FileType::YAML) => T::load_from_yaml_file(path),
-            Some(FileType::JSON) => T::load_from_json_file(path),
-            Some(FileType::CSV)  => T::load_from_csv_file(path),
-            None => bail!("Unrecognized file type: {:?}", path),
         }
     }
 
@@ -95,46 +86,81 @@ impl DataNg {
         }
     }
 
-
-    fn load_directory<T: DataTrait>(path: &Path, prefix: Option<&str>) -> SimpleResult<Vec<(String, T)>> {
+    /// Internal function to load any [`DataTrait`] type.
+    fn load<T: DataTrait>(path: &Path, prefix: Option<&str>) -> SimpleResult<Vec<(String, T)>> {
         // This is kinda clunky, but it ensures that we don't have duplicates
         // within a set
         let mut duplicates: HashSet<String> = HashSet::new();
+
+        // Catch invalid paths
+        if !path.exists() {
+            bail!("No such path: {:?}", path);
+        }
 
         WalkDir::new(path)
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| !e.file_type().is_dir())
             .map(|e| {
-                let name = Self::get_name(e.path(), prefix)?;
+                // I kept accidentally using the `path` argument
+                let path = e.path();
 
-                // Check for duplicates
+                // Get the name with prefix
+                let name = Self::get_name(path, prefix)?;
+
+                // Check for duplicates within this group
                 if duplicates.contains(&name) {
                     bail!("Duplicate name: {} (from path {:?})", name, path);
                 }
                 duplicates.insert(name.clone());
 
-                let data = Self::load_file(e.path())?;
+                // Load based on the extension
+                let data = match FileType::from_filename(path) {
+                    Some(FileType::YAML) => T::load_from_yaml_file(path)?,
+                    Some(FileType::JSON) => T::load_from_json_file(path)?,
+                    Some(FileType::CSV)  => T::load_from_csv_file(path)?,
+                    None => bail!("Unrecognized file type: {:?}", path),
+                };
 
                 Ok((name, data))
             })
             .collect()
     }
 
-    fn load_constants_dir(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<()> {
-        extend_no_duplicates(&mut self.constants, Self::load_directory(path, prefix)?)
+    /// Load constants from a [`Path`] (either a file or directory)
+    ///
+    /// Supports: YAML, CSV, JSON (based on extension)
+    pub fn load_constants(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<&Self> {
+        extend_no_duplicates(&mut self.constants, Self::load(path, prefix)?)?;
+
+        Ok(self)
     }
 
-    fn load_enums_dir(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<()> {
-        extend_no_duplicates(&mut self.enums, Self::load_directory(path, prefix)?)
+    /// Load enums from a [`Path`] (either a file or directory)
+    ///
+    /// Supports: YAML, CSV, JSON (based on extension)
+    pub fn load_enums(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<&Self> {
+        extend_no_duplicates(&mut self.enums, Self::load(path, prefix)?)?;
+
+        Ok(self)
     }
 
-    fn load_bitmasks_dir(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<()> {
-        extend_no_duplicates(&mut self.bitmasks, Self::load_directory(path, prefix)?)
+    /// Load bitmasks from a [`Path`] (either a file or directory)
+    ///
+    /// Supports: YAML, CSV, JSON (based on extension)
+    pub fn load_bitmasks(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<&Self> {
+        extend_no_duplicates(&mut self.bitmasks, Self::load(path, prefix)?)?;
+
+        Ok(self)
     }
 
-    fn load_types_dir(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<()> {
-        extend_no_duplicates(&mut self.types, Self::load_directory(path, prefix)?)
+    /// Load types from a [`Path`] (either a file or directory)
+    ///
+    /// Supports: YAML, JSON (based on extension) - does not support CSV
+    pub fn load_types(&mut self, path: &Path, prefix: Option<&str>) -> SimpleResult<&Self> {
+        extend_no_duplicates(&mut self.types, Self::load(path, prefix)?)?;
+
+        Ok(self)
     }
 }
 
@@ -147,30 +173,181 @@ mod tests {
     use simple_error::SimpleResult;
     use pretty_assertions::assert_eq;
 
+    use generic_number::Integer;
+
     #[test]
-    fn test_load_directory() -> SimpleResult<()> {
+    fn test_load_file() -> SimpleResult<()> {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/constants/test1.csv");
         let mut data = DataNg::new();
 
-        // Load the data
+        data.load_constants(&d, None)?;
+
+        // Make sure the output is sensible
+        assert_eq!(1, data.constants.len());
+        assert_eq!(0, data.enums.len());
+        assert_eq!(0, data.bitmasks.len());
+        assert_eq!(0, data.types.len());
+
+        // Load a second file
         let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        d.push("testdata/constants/");
-        data.load_constants_dir(&d, None)?;
+        d.push("testdata/constants/test2.json");
+        data.load_constants(&d, None)?;
+
+        // Make sure the output is sensible
+        assert_eq!(2, data.constants.len());
+        assert_eq!(0, data.enums.len());
+        assert_eq!(0, data.bitmasks.len());
+        assert_eq!(0, data.types.len());
+
+        // Load an enum
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/enums/test1.csv");
+        data.load_enums(&d, None)?;
+
+        // Make sure the output is sensible
+        assert_eq!(2, data.constants.len());
+        assert_eq!(1, data.enums.len());
+        assert_eq!(0, data.bitmasks.len());
+        assert_eq!(0, data.types.len());
+
+        // Correctly error on bad filename
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/NOSUCHFILE");
+        assert!(data.load_enums(&d, None).is_err());
+
+        // Check a value
+        assert_eq!(&Integer::from(100), data.constants.get("test1").unwrap().get_by_name("TEST2").unwrap());
 
         Ok(())
     }
 
     #[test]
-    fn test_load_multiple_steps() -> SimpleResult<()> {
-        todo!()
+    fn test_load_directory() -> SimpleResult<()> {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/constants/");
+        let mut data = DataNg::new();
+
+        data.load_constants(&d, None)?;
+
+        // Make sure the output is sensible
+        assert_eq!(3, data.constants.len());
+        assert_eq!(0, data.enums.len());
+        assert_eq!(0, data.bitmasks.len());
+        assert_eq!(0, data.types.len());
+
+        // Check a value
+        assert_eq!(&Integer::from(100), data.constants.get("test1").unwrap().get_by_name("TEST2").unwrap());
+
+        Ok(())
     }
 
     #[test]
-    fn test_nested() -> SimpleResult<()> {
-        todo!()
+    fn test_deeply_nested() -> SimpleResult<()> {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/nested/");
+        let mut data = DataNg::new();
+
+        data.load_constants(&d, None)?;
+
+        // Make sure the output is sensible
+        assert_eq!(1, data.constants.len());
+        assert_eq!(0, data.enums.len());
+        assert_eq!(0, data.bitmasks.len());
+        assert_eq!(0, data.types.len());
+
+        // Check a value
+        assert_eq!(&Integer::from(100), data.constants.get("constants").unwrap().get_by_name("TEST2").unwrap());
+
+        Ok(())
     }
 
     #[test]
-    fn test_ambiguous() -> SimpleResult<()> {
-        todo!()
+    fn test_prefix() -> SimpleResult<()> {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/constants/test1.csv");
+        let mut data = DataNg::new();
+
+        data.load_constants(&d, Some("MY_PREFIX"))?;
+
+        // Make sure the output is sensible
+        assert_eq!(1, data.constants.len());
+        assert_eq!(0, data.enums.len());
+        assert_eq!(0, data.bitmasks.len());
+        assert_eq!(0, data.types.len());
+
+        // Check a value
+        assert_eq!(&Integer::from(100), data.constants.get("MY_PREFIX::test1").unwrap().get_by_name("TEST2").unwrap());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_ambiguous_two_steps() -> SimpleResult<()> {
+        // Tests ambiguity from loading one, then loading a duplciate
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/constants/test1.csv");
+        let mut data = DataNg::new();
+
+        // First time works, second time fails
+        data.load_constants(&d, None)?;
+
+        assert!(data.load_constants(&d, None).is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_ambiguous_one_step() -> SimpleResult<()> {
+        // Tests ambiguity from loading a directory with two different names
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/ambiguous");
+
+        // Immediately fails
+        assert!(DataNg::new().load_constants(&d, None).is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_prefix_resolves_ambiguity() -> SimpleResult<()> {
+        // Tests ambiguity from loading one, then loading a duplciate
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/constants/test1.csv");
+        let mut data = DataNg::new();
+
+        // First time works
+        data.load_constants(&d, None)?;
+        assert_eq!(1, data.constants.len());
+
+        // Second time fails, when bare
+        assert!(data.load_constants(&d, None).is_err());
+
+        // Second time works, when we give it a name
+        data.load_constants(&d, Some("MY_PREFIX"))?;
+        assert_eq!(2, data.constants.len());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_prefix_resolves_ambiguity_directory() -> SimpleResult<()> {
+        // Tests ambiguity from loading one, then loading a duplciate
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("testdata/constants/");
+        let mut data = DataNg::new();
+
+        // First time works
+        data.load_constants(&d, None)?;
+        assert_eq!(3, data.constants.len());
+
+        // Second time fails, when bare
+        assert!(data.load_constants(&d, None).is_err());
+
+        // Second time works, when we give it a name
+        data.load_constants(&d, Some("MY_PREFIX"))?;
+        assert_eq!(6, data.constants.len());
+
+        Ok(())
     }
 }
